a411547b26c1b0be1a7da46e480b90d5ef150ad3
Fukaada <153994232+Fukaada@users.noreply.github.com>
2024-10-07 09:43:34 +0700

    Update latihan 2.py

--- latihan 2.py
+++ latihan 2.py
@@ -1,13 +1,16 @@
+print ("Selamat datang di pengecekan Sisi")
+print ("Silahkan masukan Sisi")
+#Pengecekan sisi dengan memanggil fungsi def
 def cek_sisi(sisi1, sisi2, sisi3):
     # Cek apakah ketiga sisi sama
     if sisi1 == sisi2 == sisi3:
-        return "3 sisi sama"
+        print ("3 sisi sama")
     # Cek apakah ada dua sisi yang sama
     elif sisi1 == sisi2 or sisi2 == sisi3 or sisi1 == sisi3:
-        return "2 sisi sama"
+        print ("2 sisi sama")
     # Jika tidak ada sisi yang sama
     else:
-        return "Tidak ada yang sama"
+        print ("Tidak ada yang sama")
 
 try:
     # Meminta input dari pengguna dan mengonversinya menjadi float
