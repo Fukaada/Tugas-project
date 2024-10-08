141422370c1eddf0609322caa770ce282c41cafc
Fukaada <153994232+Fukaada@users.noreply.github.com>
2024-10-08 09:36:26 +0700

    Update latihan 2.py

--- latihan 2.py
+++ latihan 2.py
@@ -10,12 +10,12 @@ def reset_terminal():  # Fungsi reset Terminal
 
 # Pengecekan sisi dengan memanggil fungsi def
 def cek_sisi(sisi1, sisi2, sisi3):
-    # Cek apakah ada dua sisi yang sama
-    if sisi1 == sisi2 or sisi2 == sisi3 or sisi1 == sisi3:
-        return "2 sisi sama"
-    # Jika semua sisi sama
-    elif sisi1 == sisi2 == sisi3:
+    # Cek apakah ketiga sisi sama
+    if sisi1 == sisi2 == sisi3:
         return "3 sisi sama"
+    # Cek apakah ada dua sisi yang sama
+    elif sisi1 == sisi2 or sisi2 == sisi3 or sisi1 == sisi3:
+        return "2 sisi sama"
     else:
         return "Tidak ada yang sama"
 
@@ -27,7 +27,7 @@ while True:
         print("Selamat datang di pengecekan Sisi")
         print("====Silahkan masukan Sisi====")
         print("Tekan tulis angka '0' disetiap sisi untuk keluar program")
-        
+
         sisi1 = float(input("Masukkan sisi 1: "))
         sisi2 = float(input("Masukkan sisi 2: "))
         sisi3 = float(input("Masukkan sisi 3: "))
